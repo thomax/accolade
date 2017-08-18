816d5b5924d11e86fdd630d3fa6c5f7432adbe2b
Thomas Drevon <thomas@bengler.no>
2017-08-18 21:54:55 +0200

    Add some extra spice to the /quality endpoint

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
 import express from 'express'
-const router = express()
 import Match from './Match'
+const router = express()
 
 router.get('/', (req, res) => {
-   res.send('Accolade sez hello')
+  res.send('Accolade sez hello')
 })
 
 router.post('/rate', (req, res) => {
@@ -15,7 +15,12 @@ router.post('/rate', (req, res) => {
 router.post('/quality', (req, res) => {
   const match = new Match(req.body)
   const result = {
-    quality: match.quality()
+    quality: match.quality(),
+    teams: match.teams.map(item => {
+      return item.team.map(player => {
+        return {id: player.id, fame: player.fame, betSize: player.betSize}
+      })
+    })
   }
   res.status(200).type('application/json').send(JSON.stringify(result, null, 2))
 })
