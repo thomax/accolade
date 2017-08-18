e5a4cb9ed1c59044bbf42f9c26c45e4622fba7db
Thomas Drevon <thomas@bengler.no>
2017-08-18 21:04:58 +0200

    Change up an endpoint

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,8 @@ import express from 'express'
 const router = express()
 import Match from './Match'
 
-router.get('/sup', (req, res) => {
-   res.send('sup?')
+router.get('/', (req, res) => {
+   res.send('Accolade sez hello')
 })
 
 router.post('/rate', (req, res) => {
@@ -19,20 +19,5 @@ router.post('/quality', (req, res) => {
   }
   res.status(200).type('application/json').send(JSON.stringify(result, null, 2))
 })
-
-// app.post('/:target', function (req, res) {
-//   var targetName = req.params.target.split(':')[0]
-//   var token = req.params.target.split(':')[1]
-//   var string = req.body.string
-//   if (string.length > 512) {
-//     return res.status(400).send('String cannot exceed 512 characters')
-//   }
-//   data.appendString(targetName, token, string, function(result) {
-//     if (result === false) {
-//       return res.status(403).send('Token mismatch')
-//     }
-//     res.type('application/json').status(201).send(result)
-//   })
-// })
 
 module.exports = router
